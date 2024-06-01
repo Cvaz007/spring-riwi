889584cbbcc4e35727b97f87b4aafcf5a275f5a5
SMoreno73 <sebastianmoreno301177@correo.itm.edu.co>
2024-06-01 14:16:37 -0500

    error handlers

---
+++ demo/src/main/java/com/example/demo/api/error_handlers/InternalServerController.java
@@ -0,0 +1,31 @@
+package com.example.demo.api.error_handlers;
+
+import com.example.demo.api.dto.request.errors.BaseErrorResponse;
+import com.example.demo.api.dto.request.errors.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.CannotCreateTransactionException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerController {
+    @ExceptionHandler(CannotCreateTransactionException.class)
+    public BaseErrorResponse handleCannotCreateTransactionException(CannotCreateTransactionException ex) {
+        return ErrorResponse.builder()
+                .message("A transaction could not be opened for the database. Please try again later.")
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public BaseErrorResponse handleGlobalException(Exception ex) {
+        return ErrorResponse.builder()
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+    }
+}
